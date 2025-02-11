4c31a1956abf09a4b9ab68261c9ecf2297cae0fe
Anmol Chawla <anmolchawla@Anmols-MacBook-Air.local>
2025-02-11 13:01:04 +0530

    added functionality to handle comma seperated input

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,4 +1,7 @@
 function add(input) {
   if (!input.length) return 0;
+  // to handle input for , seperated input
+  const result = input.split(",").map((num) => Number(num));
+  return result.reduce((curr, num) => curr + num, 0);
 }
 module.exports = add;
