336b9fc7f2427073ffe827ec542815bd9abab711
Anmol Chawla <anmolchawla@Anmols-MacBook-Air.local>
2025-02-11 13:19:33 +0530

    added functionality to handle input starting from //

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,16 @@
+function findSumWithCommaSeperatedValue(input) {
+  return input.reduce((curr, num) => curr + num, 0);
+}
 function add(input) {
   if (!input.length) return 0;
+  // to handle input starting with //
+  if (input.startsWith("//")) {
+    input = input.substring(2);
+  }
   // to handle input for , seperated input
   const result = input.split(",").map((num) => Number(num));
-  return result.reduce((curr, num) => curr + num, 0);
+  // handle input for new line \n
+  return findSumWithCommaSeperatedValue(result);
 }
+console.log(add("//1,2,3"));
 module.exports = add;
