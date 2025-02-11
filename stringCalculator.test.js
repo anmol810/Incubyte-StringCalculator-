db69b7990a9a89e6770d53a79195eb6b60760cb6
Anmol Chawla <anmolchawla@Anmols-MacBook-Air.local>
2025-02-11 13:27:12 +0530

    added test fornew line and comma seperated value

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -2,9 +2,12 @@ const add = require("./stringCalculator.js");
 test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
-test("returns the sum of numbers separated by commas", () => {
+test("returns the sum of numbers separated by commas (,)", () => {
   expect(add("1,2,3")).toBe(6);
 });
 test("returns the sum of numbers starting by //", () => {
   expect(add("//1,2,3")).toBe(6);
+});
+test("returns the sum of numbers having comma (,) and new line (\n)", () => {
+  expect(add("1\n,2,3")).toBe(6);
 });
