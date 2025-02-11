b0c64ce7ee450a72b3a737759143cf4c682205ef
Anmol Chawla <anmolchawla@Anmols-MacBook-Air.local>
2025-02-11 13:14:11 +0530

    added test for a value starting with //

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -5,3 +5,6 @@ test("returns 0 for an empty string", () => {
 test("returns the sum of numbers separated by commas", () => {
   expect(add("1,2,3")).toBe(6);
 });
+test("returns the sum of numbers starting by //", () => {
+  expect(add("//1,2,3")).toBe(6);
+});
