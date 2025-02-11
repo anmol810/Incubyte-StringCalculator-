c563784eb7e39012f8d761f206ef687167702e70
Anmol Chawla <anmolchawla@Anmols-MacBook-Air.local>
2025-02-11 12:58:39 +0530

    added test for comma seperated input

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -2,3 +2,6 @@ const add = require("./stringCalculator.js");
 test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
+test("returns the sum of numbers separated by commas", () => {
+  expect(add("1,2,3")).toBe(6);
+});
