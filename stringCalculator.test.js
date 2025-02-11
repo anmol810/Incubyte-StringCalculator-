cdb0ad94b72f4b26ad204b3a3f5e67513dd15754
Anmol Chawla <anmolchawla@Anmols-MacBook-Air.local>
2025-02-11 14:40:53 +0530

    added test to throw error if negative value is found

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -11,3 +11,6 @@ test("returns the sum of numbers having comma (,) and new line (\n)", () => {
 test("returns the sum of numbers starting with //", () => {
   expect(add("//;\n1;2")).toBe(3);
 });
+test("throws error if string has negative number", () => {
+  expect(add("1,2,-3")).toThrow("negative numbers not allowed");
+});
